a4f31a0f6e86334e62a00b6bf3d6b613cdcd6094
viannaaXz <discordvianna1@gmail.com>
2021-07-26 06:55:48 -0300

    AULA 01🔴

---
+++ AULA 01/palavras reservadas/reservadas.lua
@@ -0,0 +1,26 @@
+--[[
+Aqui mostrarei as palavras reservadas que não podemos utilizar para declarar funções e variáveis.
+
+{
+    and
+    break
+    do
+    else
+    elseif
+    end
+    false
+    for
+    function
+    if
+    in
+    local
+    nil
+    not
+    repeat
+    return
+    then
+    true
+    until
+    while
+}
+]]--
