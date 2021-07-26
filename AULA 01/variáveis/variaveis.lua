072b4e7b004355f94e1defc514d79d0c109e9c6a
viannaaXz <discordvianna1@gmail.com>
2021-07-26 06:53:14 -0300

    AULA 01🔴

---
+++ AULA 01/variáveis/variaveis.lua
@@ -0,0 +1,40 @@
+------------------------------------------------------------------------------------------------------------------------------------------
+-- VARIAVEIS GLOBAIS
+------------------------------------------------------------------------------------------------------------------------------------------
+--[[
+São variáveis que poderão ser usadas em qualquer local do código
+]]--
+
+------------------------------------------------------------------------------------------------------------------------------------------
+-- VARIAVEIS LOCAIS
+------------------------------------------------------------------------------------------------------------------------------------------
+--[[
+São variáveis que poderão ser usadas apenas em uma (function/função)
+]]--
+
+------------------------------------------------------------------------------------------------------------------------------------------
+-- TIPOS DE VARIÁVEIS
+------------------------------------------------------------------------------------------------------------------------------------------
+--[[
+    string - conjunto de PALAVRAS utilizando as ("")
+    number - variável para números
+    boolean - variável para (true/false)
+    nil - nil
+]]--
+
+local nome = "vianna" -- string
+print(nome) -- print puxando A VARIAVEL
+print("vianna2") -- print puxando o TEXTO da string
+
+local idade = 1 -- number
+print(idade)
+
+local valorA = true -- boolean
+local valorB = false -- boolean
+print(valorA)
+print(valorB)
+
+local objetoA = nil -- nil
+local objetoB -- nil
+print(objetoA)
+print(objetoB)
